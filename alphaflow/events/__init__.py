766d7a18ffe5b7ad924b1355dc07ad2a46d84df0
Brandon Schabell <brandonschabell@gmail.com>
2025-01-05 21:57:27 -0600

    Simplify imports and update example in readme

---
+++ alphaflow/events/__init__.py
@@ -0,0 +1,5 @@
+from alphaflow.events.fill_event import FillEvent
+from alphaflow.events.market_data_event import MarketDataEvent
+from alphaflow.events.order_event import OrderEvent
+
+__all__ = ["FillEvent", "MarketDataEvent", "OrderEvent"]
